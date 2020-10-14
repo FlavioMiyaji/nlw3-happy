7bacf664da341b255489c9737ffeed9bd09107dd
flavio.miyaji <flavio.miyaji@shx.com.br>
2020-10-13 22:46:04 -0300

    Creating orphanages migration/model/service

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,41 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
 
 import './database/connection';
+import Orphanage from './models/Orphanage';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.json({ message: 'Hello World' });
+app.use(express.json());
+
+app.get('/orphanages', async (request, response) => {
+  const orphanagesRepository = getRepository(Orphanage);
+  const orphanages = await orphanagesRepository.find();
+  return response.json(orphanages);
+});
+
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  } = request.body;
+  const orphanagesRepository = getRepository(Orphanage);
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends,
+  });
+  await orphanagesRepository.save(orphanage);
+  return response.status(201).json(orphanage);
 });
 
 app.listen(3333);
